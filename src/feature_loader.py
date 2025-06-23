74fb09206c20aa726d239b812936b0cd77f97b1b
devvevevv <devvkhemani6@gmail.com>
2025-06-24 00:19:46 +0530

    feat: implement feature_loader.py to wrap features.csv into PyTorch DataLoaders

---
+++ src/feature_loader.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+
+FILE_PATH = r"..\data\features.csv"
+
+class SpeechFeatureDataset(Dataset):
+    def __init__(self, features, label):
+        self.X = torch.tensor(features, dtype = torch.float32)
+        self.y = torch.tensor(label, dtype = torch.long)
+
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+def get_dataloaders(FILE_PATH):
+
+    """
+    Load the preprocessed features dataset, split it into training and testing sets,
+    and return PyTorch DataLoaders for each.
+
+    :param FILE_PATH: str, path to csv file
+    :return: pytorch DataLoaders
+    """
+
+    df = pd.read_csv(FILE_PATH)
+
+    X = df.drop(columns = ["Label"])
+    y = df["Label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+    train_set = SpeechFeatureDataset(X_train, y_train)
+    test_set = SpeechFeatureDataset(X_test, y_test)
+
+    train_loader = DataLoader(train_set, batch_size = 32, shuffle = True)
+    test_loader = DataLoader(test_set, batch_size = 32, shuffle = False)
+
+    return train_loader, test_loader
