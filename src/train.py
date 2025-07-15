2d60f17bb97bc289f3f913a636ae33dff7a92bb3
devvevevv <devvkhemani6@gmail.com>
2025-07-15 12:46:15 +0530

    feat: added lr scheduling

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ from loader import *
 from model import *
 import numpy as np
 from sklearn.utils.class_weight import compute_class_weight
+from torch.optim.lr_scheduler import StepLR
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print (f"Using device: {device}")
@@ -13,7 +14,8 @@ train_loader, _, train_labels_array = get_dataloaders()
 model = AlzhSpeechNN().to(device)
 weights = compute_class_weight('balanced', classes =np.array([0,1]), y = train_labels_array)
 criterion = torch.nn.CrossEntropyLoss(weight = torch.tensor(weights, dtype = torch.float32).to(device))
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.004, weight_decay = 1e-5)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay = 1e-5)
+scheduler = StepLR(optimizer, step_size = 30, gamma = 0.5)
 num_epochs = 60
 
 for epoch in range(num_epochs):
@@ -36,11 +38,13 @@ for epoch in range(num_epochs):
         _, predicted = torch.max(outputs, 1)
         correct += (predicted==labels).sum().item()
         total += labels.size(0)
+    scheduler.step()
 
     epoch_loss = running_loss / len(train_loader)
     epoch_accuracy = correct / total
+    current_lr = optimizer.param_groups[0]['lr']
 
-    print(f"Epoch [{epoch + 1}/{num_epochs}] | Loss: {epoch_loss:.4f} | Accuracy: {epoch_accuracy:.2f}%")
+    print(f"Epoch [{epoch + 1}/{num_epochs}] | Loss: {epoch_loss:.4f} | Accuracy: {epoch_accuracy:.2f}% | LR: {current_lr:.6f}")
 
 torch.save(model.state_dict(), MODEL_SAVE_PATH)
 print("Training complete. Model saved.")
